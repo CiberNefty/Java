979d0b954200b8354e96e7f4d5eb834ab636fe8c
CiberNefty <dsveraa.ing@gmail.com>
2023-11-28 22:36:10 -0500

    Creamos un archivo para realizar un juego de dados que contenga 13 caras

---
+++ SENA/Exercice_for_Edwin/Juego_de_Dados_13Caras.java
@@ -0,0 +1,19 @@
+public class Juego_de_Dados_13Caras {
+    public static void main(String[] args) {
+        /*
+13 jkq baraja
+tomar lo numeros del 1-10
+jqk valen  10
+condicion 1: si saca doble uno, GANA
+condicion 2: si saca una letra y el 1, GANA
+condicion 3: si saca lertra y 2, REPITE
+condicion 4: cualquira que de 14, MEDIOGANA
+
+Toca hacer unos dados de 13 caras con
+del 1 al 10 y el 11(j) 12(Q) 13(k) que ganen 
+sacando 14 o doble 1 si saca 12 que le 
+vuelva a repetir y que se salga del juego 
+presionando la tecla 9
+        */
+    }
+}
