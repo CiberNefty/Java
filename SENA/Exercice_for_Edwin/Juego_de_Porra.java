4b79316cce4a9d27409deb2e44c1e3e76cbdd286
CiberNefty <dsveraa.ing@gmail.com>
2023-12-11 23:26:27 -0500

    Creee un archivo de java nuevo en donde vamos a recrear un juego de la porra que consiste en ir eliminando elementos de una matriz.

---
+++ SENA/Exercice_for_Edwin/Juego_de_Porra.java
@@ -0,0 +1,17 @@
+public class Juego_de_Porra {
+    public static void main(String[] args) {
+     
+        System.out.println("Juego De la Porra\n\n");
+        System.out.println("Reglas del juego:\n");
+        System.out.println("1. Crear una matriz de 24 numeros hasta 50 numero");
+        System.out.println("2. Escoger cuantos participantes juegan.");
+        System.out.println("3. Cuando el usuario repite el numero se pierde.");
+        System.out.println("4. El ultimo en salir gana.");
+        System.out.println("5. El primero en salir pierde.");
+        System.out.println("6. Cada jugador un numero de la matriz.");
+        System.out.println("7. Cuando le toque un el turno a N jugador el va a digitar un numero para eliminar de la matriz.");
+
+
+        
+    }
+}
